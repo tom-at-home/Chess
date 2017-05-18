5a8f954b91217d6dc0a4d3f9664fe8affe56decf
tom <tomaszu@C-503-R2G1>
2017-05-18 12:33:30 +0200

    ADD: Neues Spiel

--- Chess/MainWindow.xaml.cs
+++ Chess/MainWindow.xaml.cs
@@ -14,16 +14,44 @@ namespace Chess
 
         Game game;
 
+        double last_top_pos;
+        double last_left_pos;
+
         public MainWindow()
         {
             InitializeComponent();
+            this.Init();
+        }
+
+        public void Init()
+        {
             game = new Game(this);
             game.Init();
+        }
+
+        public void UpdatePositionValues()
+        {
+            this.last_left_pos = this.Left;
+            this.last_top_pos = this.Top;
         }
 
         public void Select_Field(object sender, RoutedEventArgs e)
         {
             game.board.Select_Field((Square)sender);
+        }
+
+        private void New_Game(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult decision = MessageBox.Show("NEUES SPIEL STARTEN?", "BITTE BESTÄTIGEN", MessageBoxButton.YesNo);
+
+            if(decision == MessageBoxResult.Yes)
+            {
+                MainWindow mainWindow = new MainWindow();
+                this.Close();
+                mainWindow.Top = this.last_top_pos;
+                mainWindow.Left = this.last_left_pos;
+                mainWindow.Show();
+            }
         }
 
         public void Load_Game(object sender, RoutedEventArgs e)
@@ -36,13 +64,19 @@ namespace Chess
             game.Save_Game();
         }
 
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            UpdatePositionValues();
+        }
+
+        // Noch sehr experimentell
         private void Selection_DClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
             int pos = movesList.SelectedIndex;
-            string desc = this.game.logger.Get(movesList.SelectedIndex).OwnMan.Desc;
-            string cpos = this.game.logger.Get(movesList.SelectedIndex).OwnMan.Current_position;
-
-            info.Content = "Liste selektiert: " + desc + " auf " + cpos;
+            string description = this.game.logger.Get(pos).OwnMan.Desc;
+            string destination = this.game.logger.Get(pos).OwnManDest;
+            info.Content = "Liste selektiert: " + description + " auf " + destination;
         }
+
     }
 }
